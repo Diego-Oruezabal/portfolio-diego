f30f4a221fcfa6912b7af1ad0a46318e40b56596
Diego Oruezabal Mazzei <diegooruezabal@gmail.com>
2025-05-02 21:48:18 +0200

    Configuración de Resend

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,4 +1,34 @@
 // re_csNAJzAe_JNWmdgCGxkXdrSqPZMYZD79F
 
-import {Resend} from "resend"
+import { EmailTemplate } from '@/components/email.template';
+import { Resend } from 'resend';
+
+const resend = new Resend("re_csNAJzAe_JNWmdgCGxkXdrSqPZMYZD79F");
+
+export async function POST(req: Request) {
+
+    try {  
+        const dataForm = await req.json()
+        try {
+            const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: ['diegooruezabal@gmail.com'],
+            subject: 'Hello world',
+            react: EmailTemplate({
+                firstName: dataForm.username,
+                message: dataForm.message,
+                email: dataForm.email
+                }),
+                text: "DiegoDev"
+            });
+            return Response.json(data);
+         } catch (error) {
+            return Response.json({ error }, { status: 500 });
+            }
+
+            
+        } catch (error) {
+            return Response.json({ error }, { status: 500 });
+        }
+    }
 
