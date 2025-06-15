efb367e0fca45b26949f2ab1c8abb625c0c5bebc
Diego Oruezabal <diegooruezabal@gmail.com>
2025-06-15 22:01:25 +0200

    Responsive icono Idiomas

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -18,7 +18,7 @@ export default function LanguageSwitcher() {
       href={switchTo}
       className="px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary border border-red-500"
     >
-     <Image
+     <img
       src={flagSrc}
       alt={altText}
       width={35}
